5587626b38e84ba7b11083c4eaf37215985e30d2
RiaBaanu <169055318+RiaBaanu@users.noreply.github.com>
2026-04-13 20:44:03 +0800

    Delete tests/test_fusion.py

--- tests/test_fusion.py
+++
@@ -1,8 +0,0 @@
-from parsers.iot_parser import IoTCSVParser
-
-def test_iot_parser():
-    parser = IoTCSVParser("data/raw/iot_logs.csv")
-    events = parser.parse()
-
-    assert len(events) > 0
-    assert events[0].device_id == "Camera01"
